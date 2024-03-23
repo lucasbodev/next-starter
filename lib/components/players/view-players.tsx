5a0334a97c73b5adf6f3279a27bd4a76ed9c6e55
lucasbodev <lucasbodet.dev@gmail.com>
2024-03-24 00:30:48 +0100

    speed test

--- lib/components/players/view-players.tsx
+++ lib/components/players/view-players.tsx
@@ -3,7 +3,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 const ViewPlayers = () => {
     return (
         <>
-            <h1>View Players</h1>
             <SpeedInsights />
         </>
     );
