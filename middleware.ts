62cf58c41c575d4d3a3e3fb3756ba2e96b15d679
lucasbodev <lucasbodet.dev@gmail.com>
2024-11-12 15:45:31 +0100

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -10,14 +10,10 @@ export const middleware = async (request: NextRequest): Promise<NextResponse> =>
   request.cookies.set('NEXT_LOCALE', locale);
   request.nextUrl.pathname = `/${locale}${pathname}`;
   return NextResponse.redirect(request.nextUrl);
-
 };
 
 export const config = {
   matcher: [
-    // Skip all internal paths (_next, api, fr, nl, en, de)
     `/((?!_next|api|fr|nl|en|de|favicon.ico|next.svg).*)`
-    // Optional: only run on root (/) URL
-    // '/'
   ],
 };
